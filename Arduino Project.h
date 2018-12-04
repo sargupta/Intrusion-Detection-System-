7db0a7dd5e64db03eca06f7880f54ffc32444efc
sargupta <abhi.ist.15@gmail.com>
2018-12-04 16:41:58 +0000

    To view this file in txt format, click on Arduino Project.txt file.

---
+++ Arduino Project.h
@@ -0,0 +1,123 @@
+/*
+===Hardware===
+- Arduino Uno R3
+- MPU-6050 
+- Arduino Wire library
+*/
+
+#include <Wire.h>
+
+const double accelThreshold = 0.6;
+int waterLevel;
+const double waterThreshold = 250;
+
+int LEDyellow = 9;
+
+
+long accelX, accelY, accelZ;
+float gForceX, gForceY, gForceZ;
+
+long gyroX, gyroY, gyroZ;
+float rotX, rotY, rotZ;
+
+int LEDgreen = 13;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  setupMPU();
+  pinMode(LEDgreen, OUTPUT);
+  pinMode(LEDyellow, OUTPUT);
+}
+
+
+void loop() {
+  recordAccelRegisters();
+  recordGyroRegisters();
+  printData();
+  double totalAccel = sqrt(pow((double) gForceX, 2.)+pow((double) gForceY, 2.)+pow((double) gForceZ-0.86, 2.));
+  Serial.println(totalAccel);
+  if(totalAccel>accelThreshold)
+    digitalWrite(LEDgreen, HIGH);
+  else
+    digitalWrite(LEDgreen, LOW);
+  delay(100);
+  
+  
+  Serial.println(analogRead(A0));
+	waterLevel = analogRead(A0);
+	if(waterLevel>waterThreshold)
+    digitalWrite(LEDyellow, HIGH);
+  else
+    digitalWrite(LEDyellow, LOW);
+  delay(100);
+}
+
+void setupMPU(){
+  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet)
+  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
+  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required)
+  Wire.endTransmission();  
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration  
+  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s 
+  Wire.endTransmission(); 
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration 
+  Wire.write(0b00000000); //Setting the accel to +/- 2g
+  Wire.endTransmission(); 
+}
+
+void recordAccelRegisters() {
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x3B); //Starting register for Accel Readings
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101000,6); //Request Accel Registers (3B - 40)
+  while(Wire.available() < 6);
+  accelX = Wire.read()<<8|Wire.read(); //Store first two bytes into accelX
+  accelY = Wire.read()<<8|Wire.read(); //Store middle two bytes into accelY
+  accelZ = Wire.read()<<8|Wire.read(); //Store last two bytes into accelZ
+  processAccelData();
+}
+
+void processAccelData(){
+  gForceX = accelX / 16384.0;
+  gForceY = accelY / 16384.0; 
+  gForceZ = accelZ / 16384.0;
+}
+
+void recordGyroRegisters() {
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x43); //Starting register for Gyro Readings
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101000,6); //Request Gyro Registers (43 - 48)
+  while(Wire.available() < 6);
+  gyroX = Wire.read()<<8|Wire.read(); //Store first two bytes into accelX
+  gyroY = Wire.read()<<8|Wire.read(); //Store middle two bytes into accelY
+  gyroZ = Wire.read()<<8|Wire.read(); //Store last two bytes into accelZ
+  processGyroData();
+}
+
+void processGyroData() {
+  rotX = gyroX / 131.0;
+  rotY = gyroY / 131.0; 
+  rotZ = gyroZ / 131.0;
+}
+
+void printData() {
+  Serial.print("Gyro (deg)");
+  Serial.print(" X=");
+  Serial.print(rotX);
+  Serial.print(" Y=");
+  Serial.print(rotY);
+  Serial.print(" Z=");
+  Serial.print(rotZ);
+  Serial.print(" Accel (g)");
+  Serial.print(" X=");
+  Serial.print(gForceX);
+  Serial.print(" Y=");
+  Serial.print(gForceY);
+  Serial.print(" Z=");
+  Serial.println(gForceZ);
+}
+
